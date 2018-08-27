6badaff267bd59f50e706304b26e0b3d7ae882fc
Driver <peplinskikrzysztof@gmail.com>
2018-08-27 20:02:00 +0200

    added visits, updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/pl/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/petclinic/model/Owner.java
@@ -1,5 +1,20 @@
 package pl.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Owner(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
