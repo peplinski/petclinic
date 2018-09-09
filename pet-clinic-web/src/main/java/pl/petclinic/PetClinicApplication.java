3cfe9eec0a3ee883f5ebfe2ed0b4c00508231363
Driver <peplinskikrzysztof@gmail.com>
2018-09-09 09:37:05 +0200

    Upgraded to JUnit 5. Closes #4

--- pet-clinic-web/src/main/java/pl/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/pl/petclinic/PetClinicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "pl.petclinic")
+//@ComponentScan(basePackages = "pl.petclinic")
 public class PetClinicApplication {
 
     public static void main(String[] args) {
--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -1,16 +1,19 @@
 package petclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-/*@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ContextConfiguration(classes = {PetClinicApplicationTests.class})
 public class PetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
     }
 
-}*/
+}
